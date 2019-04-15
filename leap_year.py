08e25d5005cb1035d25fff8a75a2323abe275c75
mherna42 <49410553+mherna42@users.noreply.github.com>
2019-04-15 14:16:00 -0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -5,4 +5,13 @@
 # Don't forget to reach out for help after your own due diligence
 
 def is_leap_year(year):
-    return ''
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
